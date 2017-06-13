4dd9d3a435eec5a7b188d594a81a498f1d703735
fantaandcrackers <arthur.shir@gmail.com>
2017-06-13 11:54:20 -0700

    Added jqueyr loading code

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
-$( document ).ready(function() {
-  document.body.style.backgroundColor = "red";
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
 
-
-    $("#iframe").on("load", function () {
-        // var $head = $("#iframe").contents().find("head"); 
-
-        var url = "{% static 'shared/stylesheets/base.css' %}";
-        $("head").append($("<link/>", { rel: "stylesheet", href: url, type: "text/css" } ));
-    });
-});
