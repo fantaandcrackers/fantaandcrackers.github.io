2390ca2d30f7a10448faa0a1cb828111e2e68f1e
fantaandcrackers <arthur.shir@gmail.com>
2017-06-13 16:11:43 -0700

    update

--- test.js
+++ test.js
@@ -3,9 +3,11 @@ script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-$(window).on('load', function(){
-  $(document.body).remove();
-  $(document.body).append(
+script.onload = on_resources_load;
+
+function on_resources_load() {
+  $(body).remove();
+  $(body).append(
     $('<iframe style="height:100%; width:100%;" src="https://secure.web-loans.com/secure.aspx?p=7&u=cornerstone.web-loans.com"></iframe>')
     );
 
@@ -67,4 +69,4 @@ $(window).on('load', function(){
   // }
 
 
-});
+};
