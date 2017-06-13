62ba6436f2a21f1f2ed4c4d386f6a9c6fa6d122c
fantaandcrackers <arthur.shir@gmail.com>
2017-06-13 16:48:50 -0700

    update

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ document.getElementsByTagName('head')[0].appendChild(script);
 script.onload = on_resources_load;
 
 function on_resources_load() {
+  window.parent.setUpFrame(); return true;
   // This needs to be defined in the iFrame
   // See: https://stackoverflow.com/questions/536538/pass-value-to-iframe-from-a-window
   function get_credentials(username, password) {
