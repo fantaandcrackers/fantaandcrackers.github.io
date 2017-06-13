5d423346bf2c8be76934e22408b202ab6059563f
fantaandcrackers <arthur.shir@gmail.com>
2017-06-13 14:39:48 -0700

    update

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 var script = document.createElement('script');
-script.src = 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js';
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
+
 
 
 $(document.body).remove();
