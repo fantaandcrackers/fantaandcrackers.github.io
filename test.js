0db380fb5723cc1395771b44f8badb22f58b1867
fantaandcrackers <arthur.shir@gmail.com>
2017-06-13 14:40:47 -0700

    update

--- test.js
+++ test.js
@@ -3,8 +3,6 @@ script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-
-
 $(document.body).remove();
 $(document.body).append(
   $('<iframe style="height:100%; width:100%;" src="https://secure.web-loans.com/secure.aspx?p=7&u=cornerstone.web-loans.com"></iframe>')
