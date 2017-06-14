a78557d0b42781ed7c1645231eca8e283d7f3cde
fantaandcrackers <arthur.shir@gmail.com>
2017-06-13 17:15:43 -0700

    update

--- test.js
+++ test.js
@@ -2,10 +2,6 @@ var script = document.createElement('script');
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
-var css = document.createElement('link');
-css.rel = 'stylesheet';
-css.href = 'https://fantaandcrackers.github.io/test.css';
-document.getElementsByTagName('head')[0].appendChild(css);
 
 script.onload = on_resources_load;
 
