19df86acf10e8b225a6fddb0c08d171090385cc4
Arthur Shir <fantaandcrackers@users.noreply.github.com>
2017-06-19 15:36:40 -0700

    Update test.js

--- test.js
+++ test.js
@@ -6,9 +6,12 @@ document.getElementsByTagName('head')[0].appendChild(script);
 script.onload = on_resources_load;
 
 function on_resources_load() {
-  var encoded_hash = $(location).attr('hash').slice(1);
-  credentials = atob(encoded_hash).split(',')
-  $('#txtEmailAddress').val(credentials[0]);
-  $('#txtPassword').val(credentials[1]);
-  $('#butImageLogin').click();
+  var hash = $(location).attr('hash').slice;
+  if (hash) {
+    encoded_hash = hash.slice(5);
+    credentials = atob(encoded_hash).split(',')
+    $('#txtEmailAddress').val(credentials[0]);
+    $('#txtPassword').val(credentials[1]);
+    $('#butImageLogin').click();
+  };
 };
